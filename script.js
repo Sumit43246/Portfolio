694ab3eb7312545c57520a6ecc88ed41f876b085
Sumit Srivastav <srivastavsumit012@gmail.com>
2024-06-19 16:59:01 +0000

    done..

--- script.js
+++ script.js
@@ -33,9 +33,9 @@ window.onscroll = () => {
     /*==================== sticky navbar ====================*/
        let header = document.querySelector('header');
 
-       header.classList.toggle('sticky' , window.scrollY > 100);
+       header.classList.toggle('sticky', window.scrollY > 100);
        
-    /*==================== remove to ggle icon and navbar when click navbar link (scroll) ====================*/
+    /*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
 };
@@ -78,20 +78,20 @@ function toggelText(){
 /*==================== circle skill ====================*/
 
 
-const circle = document.querySelectorAll('.circle');
-circle.forEach(elem=>{
-  var dots = elem.getAttribute("data-dots");
-  var marked = elem.getAttribute("data-percent");
-  var percent = Math.floor(dots*marked/100);
-  var points = "";
-  var rotate = 360 / dots;
+ const circle = document.querySelectorAll('.circle');
+ circle.forEach(elem=>{
+    var dots = elem.getAttribute("data-dots");
+    var marked = elem.getAttribute("data-percent");
+    var percent = Math.floor(dots*marked/100);
+    var points = "";
+    var rotate = 360 / dots;
 
   for(let i = 0; i < dots ; i++){
-      points += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
-    }
-    elem.innerHTML = points;
-    const pointsMarked = elem.querySelectorAll('.points');
-    for(let i = 0; i<percent; i++){
+       points += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
+     }
+     elem.innerHTML = points;
+     const pointsMarked = elem.querySelectorAll('.points');
+  for(let i = 0; i<percent; i++){
     pointsMarked[i].classList.add('marked')
     }
-})
+ })
