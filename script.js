a478a227494008631b08a9fd035f537e1f17841a
Sumit Srivastav <srivastavsumit012@gmail.com>
2024-06-20 08:19:06 +0000

    JAVASCRIPT  DONE..

--- script.js
+++ script.js
@@ -91,10 +91,9 @@ function toggelText(){
   for(let i = 0; i < dots ; i++){
        points += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
      }
-     
      elem.innerHTML = points;
      const pointsMarked = elem.querySelectorAll('.points');
   for(let i = 0; i<percent; i++){
-    pointsMarked[i].classList.add('marked'
+    pointsMarked[i].classList.add('marked')
     }
  })
